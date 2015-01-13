8be297a3b45464d3c9109aa23ff080602b3ceec9
Connor Kojimoto <cokojimoto@gmail.com>
2015-01-12 20:24:49 -0800

    can now update database quantity

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,11 +2,14 @@ var app = angular.module("AmazonApp",["ngResource"]);
 
 app.controller("MainCtrl",["$scope", "$resource",function($scope, $resource) {
 
-  var Item = $resource('/items')    //{id: "@id"}, {update: {method: "PATCH"}});
+  var Item = $resource('/items/:id',{id: "@id"}, {update: {method: "PATCH"}});
+
+
+  // var PlaceOrder = $resource("/items/:id",)
 
   Item.query(function(items) { console.log(items); });
-
-  $scope.items = Item.query();
+  
+  $scope.items = Item.query(); 
 
   // $scope.items = Item.get({id: 5});
 
@@ -18,6 +21,7 @@ app.controller("MainCtrl",["$scope", "$resource",function($scope, $resource) {
 
 
   $scope.cart = {items:[],total:0};
+  $scope.name = "";
 
   var lowerQuantity = function(name) {
     $scope.items.forEach(function(item) {
@@ -30,25 +34,68 @@ app.controller("MainCtrl",["$scope", "$resource",function($scope, $resource) {
     });
   };
 
-  $scope.addToCart = function(name,price) {
+  var updateQuantity = function(newQuantity, id) {
+    console.log(newQuantity+" "+id);
+    var updatedItem = Item.get({id:id}, function(item) {
+      item.quantity -= newQuantity;
+      item.$update();
+      console.log(item);
+    });
+    // console.log(updatedItem);
+  } 
+
+  $scope.placeOrder = function() {
+    $scope.cart.name = $scope.name;
+    $scope.cart.items.forEach(function(item) {
+      console.log(item)
+      updateQuantity(item.quantity,item.id)
+    });
+    
+  };
+
+  $scope.addToCart = function(item) {
+    var price = item.price;
+    var name = item.name;
+    var id = item.id;
     $scope.cart.total += price;
     lowerQuantity(name);
     var added = false;
     if ($scope.cart.items.length < 1) {
-      $scope.cart.items.push({name:name,price:price,quantity:1});
+      $scope.cart.items.push({name:name,price:price,quantity:1,id:id});
     } else {  
       var len = $scope.cart.items.length;
       for (var i = 0; i <= len; i++) {
         if(i === len && added === false) {
-          $scope.cart.items.push({name:name,price:price,quantity:1});
+          $scope.cart.items.push({name:name,price:price,quantity:1,id:id});
         } else if($scope.cart.items[i].name == name) {
           $scope.cart.items[i].quantity++;
           added = true;
         } 
       }
+    }        
+  };
+
+  var increaseQuantity = function(name, quantity) {
+    $scope.items.forEach(function(item) {
+      if (item.name === name) {
+        item.quantity += quantity;
+      }
+    });
+  };
+
+  $scope.removeFromCart = function(name,price) {
+     var price = 0;
+    
+    var subtracted = false;
+    for (var i = 0; i < $scope.cart.items.length; i++) {
+      if($scope.cart.items[i].name === name) {
+        var res = $scope.cart.items.splice(i,1);
+        increaseQuantity(name,res[0].quantity);
+        price = res[0].quantity * res[0].price;
+      }
     }
 
-        
+    $scope.cart.total -= price
   };
     
 }]);
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :orders
 
   root 'items#home'
-  resources :items
+  resources :items, defaults: {format: :json}
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
